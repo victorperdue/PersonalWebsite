6c74da5b7fac4abd8c10412cc6bbab82be54314c
victorperdue <victor.perdue@yahoo.com>
2024-07-26 19:19:16 -0700

    dark mode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
 console.log("Hello, World!")
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.getElementById('dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme');
+  
+    if (currentTheme) {
+      document.body.classList.add(currentTheme);
+  
+      if (currentTheme === 'dark-mode') {
+        toggleSwitch.checked = true;
+      }
+    }
+  
+    toggleSwitch.addEventListener('change', () => {
+      if (toggleSwitch.checked) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+        localStorage.setItem('theme', 'dark-mode');
+      } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light-mode');
+      }
+    });
+  });
