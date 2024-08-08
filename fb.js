9f743623390e2d8ed55fea7658a7b30dfec0194f
victorperdue <victor.perdue@yahoo.com>
2024-08-07 18:13:04 -0700

    fb

---
+++ fb.js
@@ -0,0 +1,13 @@
+let num = 15
+
+for (i = 1; i <= num; i++) {
+    if (i % 3 ===0 && i % 5 ===0) {
+        console.log("fizzBuzz");
+    } else if (i % 3 ===0) {
+        console.log("fizz");
+    } else if (i % 5 ===0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    }
+}
