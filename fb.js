8dac41ea9ccb7cd5e8de5148d1843fbca0e78ae9
victorperdue <victor.perdue@yahoo.com>
2024-08-10 15:27:36 -0700

    car

--- fb.js
+++ fb.js
@@ -6,13 +6,22 @@
 // console.log(isPalindrome("racecar")); // Output: true
 // console.log(isPalindrome("hello"));
 
-function isPalindrome (str) {
-    const reversed = str.split("").reverse("").join("");
-    return str === reversed;
-}
+// function isPalindrome (str) {
+//     const reversed = str.split("").reverse("").join("");
+//     return str === reversed;
+// }
 
-console.log(isPalindrome("545"));
-console.log(isPalindrome("toby"));
+// console.log(isPalindrome("545"));
+// console.log(isPalindrome("toby"));
+ const car = {
+    color: "red",
+    year: 1999,
+    isFast: true
+ };
+car.color= "blue";
+ console.table(car);
+ 
+ 
 
 
 
