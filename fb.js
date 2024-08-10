1fdc5c37b42a6c9c67be4a68164ce014ba218b84
victorperdue <victor.perdue@yahoo.com>
2024-08-09 17:33:11 -0700

    rvrsstr

--- fb.js
+++ fb.js
@@ -1,13 +1,51 @@
-let num = 15
+// let num = 15
 
-for (i = 1; i <= num; i++) {
-    if (i % 3 ===0 && i % 5 ===0) {
-        console.log("fizzBuzz");
-    } else if (i % 3 ===0) {
-        console.log("fizz");
-    } else if (i % 5 ===0) {
-        console.log("buzz");
-    } else {
-        console.log(i);
-    }
+const { unstable_ClassNameGenerator } = require("@mui/material");
+
+// for (i=1;i<num;i++) {
+//     if(i%3===0&&i%5===0){
+//         console.log("fizzBuzz");
+//     } else if (i%3===0) {
+//         console.log("fizz");
+//     } else if (i%5===0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// function reverseString (str) {
+//     return str.split("").reverse("").join("")
+// }
+// console.log(reverseString("hello"));
+
+// const dogname = (name) => {
+//     console.log(`${name} is a good pup`);
+// } 
+// dogname ("vic");
+// dogname ("bud");
+
+// function add (a, b) {
+//     return a + b;
+// }
+// console.log(add(2, 3));
+
+// //Random Num Generator
+// function radNum () {
+//     let num = Math.random() * 3;
+//     num = Math.ceil(num);
+//     return num;
+// }
+// console.log("random number:", radNum());
+
+function reverseString(str) {
+    return str.split("").reverse("").join("");
 }
+console.log(reverseString("victor"));
+
+
+
+
+
+
+
