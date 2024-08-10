e284bcd507c76b2c424154717f0669abf32b25ca
victorperdue <victor.perdue@yahoo.com>
2024-08-10 15:21:43 -0700

    palin

--- fb.js
+++ fb.js
@@ -1,47 +1,25 @@
-// let num = 15
-
-const { unstable_ClassNameGenerator } = require("@mui/material");
-
-// for (i=1;i<num;i++) {
-//     if(i%3===0&&i%5===0){
-//         console.log("fizzBuzz");
-//     } else if (i%3===0) {
-//         console.log("fizz");
-//     } else if (i%5===0) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(i);
-//     }
+// function isPalindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+//     return str === reversed;
 // }
 
-// function reverseString (str) {
-//     return str.split("").reverse("").join("")
-// }
-// console.log(reverseString("hello"));
+// console.log(isPalindrome("racecar")); // Output: true
+// console.log(isPalindrome("hello"));
 
-// const dogname = (name) => {
-//     console.log(`${name} is a good pup`);
-// } 
-// dogname ("vic");
-// dogname ("bud");
-
-// function add (a, b) {
-//     return a + b;
-// }
-// console.log(add(2, 3));
-
-// //Random Num Generator
-// function radNum () {
-//     let num = Math.random() * 3;
-//     num = Math.ceil(num);
-//     return num;
-// }
-// console.log("random number:", radNum());
-
-function reverseString(str) {
-    return str.split("").reverse("").join("");
+function isPalindrome (str) {
+    const reversed = str.split("").reverse("").join("");
+    return str === reversed;
 }
-console.log(reverseString("victor"));
+
+console.log(isPalindrome("545"));
+console.log(isPalindrome("toby"));
+
+
+
+
+
+
+
 
 
 
