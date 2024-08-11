8dc47a96e41d530197274a85542476f462be8fbf
victorperdue <victor.perdue@yahoo.com>
2024-08-11 10:45:57 -0700

    oddsEvens

--- fb.js
+++ fb.js
@@ -1,42 +1,77 @@
+//FizzBuzz
+// let num =i
+
+// for (i=1; i<=num;i++) {
+//     if (i%3===0 && i%5===0) {
+//         console.log("fizzBuzz");
+//     } else if (i%3===0) {
+//         console.log("fizz");
+//     } else if (i%5===0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+//Is palindrome or nah?
 // function isPalindrome(str) {
 //     const reversed = str.split('').reverse().join('');
 //     return str === reversed;
 // }
-
 // console.log(isPalindrome("racecar")); // Output: true
-// console.log(isPalindrome("hello"));
+// console.log(isPalindrome("hello")); // Output: false
 
-// function isPalindrome (str) {
-//     const reversed = str.split("").reverse("").join("");
-//     return str === reversed;
+
+// reverse string
+// function reverseString (str) {
+//     return str.split("").reverse("").join("")
 // }
+// console.log(reverseString("hello"));
 
-// console.log(isPalindrome("545"));
-// console.log(isPalindrome("toby"));
- const car1 = {
-    color: "red",
-    year: 1995,
-    isFast: true
- };
+
+//Created 2 objects and destructered them 
+//  const car1 = {
+//     color: "red",
+//     year: 1995,
+//     isFast: true
+//  };
  
- const car2 = {
-    color: "blue",
-    year: 1996,
-    isFast: true
- };
+//  const car2 = {
+//     color: "blue",
+//     year: 1996,
+//     isFast: true
+//  };
 
- const {color, year, isFast} = car2
-
- console.log(car2);
+//  const {color, year, isFast} = car2
+//  console.log(car2);
  
+
+// how to loop through the object below
 // function printObj(obj) {
 //     for (const key in obj) {
 //         console.log(`key: ${key}, value: ${obj[key]}`);
-        
 //     }
 // } 
-
 // printObj(car);
+
+
+// creating an Array, an object then looping through array splitting them into odds and evens
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// const oddsEvens = {
+//     odds: [],
+//     evens: [],
+// }
+
+// for (let i=0; i < arr.length; i++) {
+//     if (arr[i] %2 ===1) {
+//         oddsEvens["odds"].push(arr[i]);
+//     } else {
+//         oddsEvens.evens.push(arr[i]);
+//     }
+// }
+// console.log(oddsEvens);
 
  
  
